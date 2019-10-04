1b7b081b689e6e3ff739bb70f3faa683d42b052b
Eric <eoster2015>
2019-10-04 14:38:55 -0400

    Implemented Experiment 1 to test the difference in efficiency between RSA decryption with and without CRT acceleration.

---
+++ experiment_1_macos.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+
+from scipy import stats
+
+import experiment_methods as exp
+import rsa_methods as rsa
+
+
+def main():
+    print()
+    print("Experiment 1: Rivest-Shamir-Adelson Decryption Acceleration with Chinese Remainder Theorem\n", sep="")
+
+    key_bits = 1024
+    message_length = 256
+    num_of_trials = 10
+    ns_conversion = 10**9
+
+    print("Generating random ", key_bits, " bit RSA keys...", sep="")
+    public_key, private_key = rsa.rsa_key_generation(key_bits)
+    n, e = public_key
+    p, q, d = private_key
+    print("Public key = ", public_key, sep="")
+    print("Private key = ", private_key, "\n", sep="")
+
+    print("Generating a random string to be encrypted using RSA...", sep="")
+    message = exp.generate_random_string(message_length)
+    print("Test Phrase = ", message, "\n", sep="")
+
+    print("Encrypting test phrase using RSA...", sep="")
+    ciphertext = rsa.rsa_encryption(message, public_key)
+    print("Ciphertext = ", ciphertext, "\n", sep="")
+
+    print("Testing RSA decryption time without using CRT...", sep="")
+    no_crt_decrypt_timings = []
+    for i in range(num_of_trials):
+        current_start_time = time.time()
+        current_decrypted_message = rsa.rsa_no_crt_decryption(ciphertext, public_key, private_key)
+        if current_decrypted_message == message:
+            current_end_time = time.time()
+            current_time = current_end_time - current_start_time
+            no_crt_decrypt_timings.append(current_time)
+            print("Trial ", i+1, " = ", current_time, " seconds", sep="")
+        else:
+            print("ERROR: decrypted ciphertext does not match original message", sep="")
+            sys.exit(0)
+    print()
+
+    print("Testing RSA decryption time using CRT acceleration...", sep="")
+    crt_decrypt_timings = []
+    for i in range(num_of_trials):
+        current_start_time = time.time()
+        current_decrypted_message = rsa.rsa_crt_decryption(ciphertext, private_key)
+        if current_decrypted_message == message:
+            current_end_time = time.time()
+            current_time = current_end_time - current_start_time
+            crt_decrypt_timings.append(current_time)
+            print("Trial ", i + 1, " = ", current_time, " seconds", sep="")
+        else:
+            print("ERROR: decrypted ciphertext does not match original message", sep="")
+            sys.exit(0)
+    print()
+
+    print("Experiment Results:")
+    print("RSA decryption times without CRT acceleration (in seconds): ", no_crt_decrypt_timings, sep="")
+    print("RSA decryption times with CRT acceleration (in seconds): ", crt_decrypt_timings, "\n", sep="")
+    no_crt_decrypt_avg = sum(no_crt_decrypt_timings) / len(no_crt_decrypt_timings)
+    crt_decrypt_avg = sum(crt_decrypt_timings) / len(crt_decrypt_timings)
+    percent_improvement = 100 - ((crt_decrypt_avg / no_crt_decrypt_avg) * 100)
+    print("Average RSA decryption time without CRT acceleration = ", format(no_crt_decrypt_avg, "0.3f"), " seconds", sep="")
+    print("Average RSA decryption time with CRT acceleration = ", format(crt_decrypt_avg, "0.3f"), " seconds", sep="")
+    print("Percent improvement by CRT acceleration = ", format(percent_improvement, "0.3f"), "%\n", sep="")
+    t, p = stats.ttest_ind(no_crt_decrypt_timings, crt_decrypt_timings)
+    print("p-value = ", p, sep="")
+    if p < 0.01:
+        print("The results of this experiment are statistically significant for p < 0.01")
+    else:
+        print("The results of this experiment are not statistically significant")
+    print()
+
+
+if __name__ == "__main__":
+    main()
